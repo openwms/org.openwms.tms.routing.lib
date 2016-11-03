07ace97450ada44681335755b31ac8a9b4c5037d
Heiko Scherrer <scherrer@openwms.org>
2016-11-03 22:39:14 +0100

    Locations need to belong to a LocationGroup

--- src/main/java/org/openwms/tms/routing/RoutingServiceRunner.java
+++ src/main/java/org/openwms/tms/routing/RoutingServiceRunner.java
@@ -30,6 +30,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * A RoutingServiceRunner.
@@ -39,6 +40,7 @@ import org.springframework.context.annotation.Profile;
 @SpringBootApplication(scanBasePackageClasses = SolutionApp.class, scanBasePackages = "org.openwms")
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableTransactionManagement
 @EntityScan(basePackages = "org.openwms")
 public class RoutingServiceRunner {
 
