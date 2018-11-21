d3921adbfa04b4e4c43eba5636685d5d25670be9
Heiko Scherrer <scherrer@openwms.org>
2018-11-21 20:41:53 +0100

    #24 merged message handler implementations in order to become more generic in TMS routing - in COMM we need to pass the message type in order to get generic on routing side - UPD and REQ handling was almost the same, just merged it. SYSU is still separate and needs more investigation even it is more simple than others.

---
+++ src/main/java/org/openwms/common/comm/ItemMessageHandler.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2018 Heiko Scherrer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openwms.common.comm;
+
+import org.ameba.annotation.TxService;
+import org.ameba.exception.NotFoundException;
+import org.openwms.common.LocationGroupVO;
+import org.openwms.common.LocationVO;
+import org.openwms.common.location.api.LocationApi;
+import org.openwms.common.location.api.LocationGroupApi;
+import org.openwms.tms.TransportOrder;
+import org.openwms.tms.api.TransportOrderApi;
+import org.openwms.tms.routing.InputContext;
+import org.openwms.tms.routing.Matrix;
+import org.openwms.tms.routing.NoRouteException;
+import org.openwms.tms.routing.ProgramExecutor;
+import org.openwms.tms.routing.Route;
+import org.openwms.tms.routing.RouteSearchAlgorithm;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+import static java.lang.String.format;
+
+/**
+ * A RequestMessageHandler.
+ *
+ * @author <a href="mailto:scherrer@openwms.org">Heiko Scherrer</a>
+ */
+@TxService
+public class ItemMessageHandler {
+
+    private final Matrix matrix;
+    private final ProgramExecutor executor;
+    private final InputContext in;
+    private final RouteSearchAlgorithm routeSearch;
+    private final LocationApi locationApi;
+    private final LocationGroupApi locationGroupApi;
+    private final TransportOrderApi transportOrderApi;
+
+    ItemMessageHandler(Matrix matrix, ProgramExecutor executor, InputContext in, RouteSearchAlgorithm routeSearch, LocationApi locationApi, LocationGroupApi locationGroupApi, TransportOrderApi transportOrderApi) {
+        this.matrix = matrix;
+        this.executor = executor;
+        this.in = in;
+        this.routeSearch = routeSearch;
+        this.locationApi = locationApi;
+        this.locationGroupApi = locationGroupApi;
+        this.transportOrderApi = transportOrderApi;
+    }
+
+    public void handle(ItemMessage msg) {
+        Assert.notNull(msg, "handle called with null message");
+        Assert.notNull(msg.getHeader(), "handle called without message header");
+        in.putAll(msg.getAll());
+        in.putAll(msg.getHeader().getAll());
+
+        LocationVO actualLocation = locationApi
+                .findLocationByCoordinate(msg.getActualLocation())
+                .orElseThrow(()-> new NotFoundException(format("Location with coordinate [%s] does not exist", msg.getActualLocation())));
+
+        LocationGroupVO locationGroup =
+                msg.hasLocationGroupName() ?
+                        locationGroupApi.findByName(msg.getLocationGroupName())
+                                .orElseThrow(()->new NotFoundException("No LocationGroup exists for handling REQ message in routing")) :
+                        locationGroupApi.findByName(actualLocation.getLocationGroupName())
+                                .orElseThrow(()->new NotFoundException("No LocationGroup exists for handling REQ message in routing"));
+
+        Route route = Route.NO_ROUTE;
+        List<TransportOrder> transportOrders = transportOrderApi.findBy(msg.getBarcode(), "STARTED");
+        if (transportOrders != null && !transportOrders.isEmpty()) {
+            TransportOrder transportOrder = transportOrders.get(0);
+            in.putAll(transportOrder.getAll());
+            try {
+                route = routeSearch.findBy(transportOrder.getSourceLocation(), transportOrder.getTargetLocation(), transportOrder.getTargetLocationGroup());
+            } catch (NoRouteException nre) {
+                // perfectly fine here
+            }
+        }
+        in.put("route", route);
+        executor.execute(matrix.findBy(msg.getType(), route, actualLocation, locationGroup), in.getMsg());
+    }
+}
